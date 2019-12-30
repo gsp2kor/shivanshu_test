bc07a36268e5bbe6b000d04b8d1df7a2d86592af
Shivanshu_Gupta <gupta_shivanshu@yahoo.co.in>
2019-12-30 00:23:10 -0600

    RemoveDuplicatesInArray - Modified

--- java-practice/src/main/java/com/shivanshu/practice/RemoveDuplicatesInArray.java
+++ java-practice/src/main/java/com/shivanshu/practice/RemoveDuplicatesInArray.java
@@ -10,40 +10,65 @@ public class RemoveDuplicatesInArray {
     }
 
     private static int[] removeDuplicatesModifyInputArray(int[] sortedArray){
-        if(sortedArray == null) throw new RuntimeException("Array can't be null");
-        if(sortedArray.length<2) return sortedArray;
-        int i =1;
-        int j =0;
-        while(i<sortedArray.length){
-            if(sortedArray[i] == sortedArray[j]){
+
+        int i = 0;
+        for (int j = 1; j < sortedArray.length; j++) {
+            if(sortedArray[j] != sortedArray[i]){
                 i++;
-            }else {
-                sortedArray[++j] = sortedArray[i++];
+                sortedArray[i] = sortedArray[j];
             }
         }
-        return Arrays.copyOfRange(sortedArray,0,j+1);
+        return Arrays.copyOfRange(sortedArray,0,i+1);
     }
 
     private static int[] removeDuplicatesNotModifyingInput(int[] sortedArray){
-        if(sortedArray == null) throw new RuntimeException("Array can't be null");
-        if(sortedArray.length<2) return sortedArray;
-        int i =1;
-        int j =0;
-        int[] result = new int[sortedArray.length];
-        result[0] = sortedArray[0];
-        while(i<sortedArray.length){
-            if(sortedArray[i] == result[j]){
+        int[] resultArr = new int[sortedArray.length];
+        resultArr[0] = sortedArray[0];
+        int i = 0;
+        for (int j = 1; j < sortedArray.length; j++) {
+            if(resultArr[i] != sortedArray[j]){
                 i++;
-            }else {
-                result[++j] = sortedArray[i++];
+                resultArr[i] = sortedArray[j];
             }
         }
-        return Arrays.copyOfRange(result,0,j+1);
+        return Arrays.copyOfRange(resultArr,0,i+1);
+    }
+
+    private static int[] removeDuplicatedElement(int[] sortedArray){
+
+        int i = 0;
+        int j = 0;
+        while(j<sortedArray.length){
+            if(sortedArray[j] == sortedArray[j+1]){
+                int temp = sortedArray[j];
+                while (j<sortedArray.length && sortedArray[j] == temp){
+                    j++;
+                }
+            }else {
+                sortedArray[i++] =  sortedArray[j++];
+            }
+
+        }
+        return Arrays.copyOfRange(sortedArray,0,i);
+    }
+
+    private static int[] removeElement(int[] sortedArray, int val){
+
+        int i = 0;
+        for (int j = 0; j < sortedArray.length; j++) {
+            if(sortedArray[j] != val){
+                sortedArray[i] = sortedArray[j];
+                i++;
+            }
+        }
+        return Arrays.copyOfRange(sortedArray,0,i);
     }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(removeDuplicatesJava8(new int[]{-1,-1,0,0,2,3,3,4,7,9,9,13,15,15,15,15,15,15})));
         System.out.println(Arrays.toString(removeDuplicatesModifyInputArray(new int[]{-1,-1,0,0,2,3,3,4,7,9,9,13,15,15,15,15,15,15})));
         System.out.println(Arrays.toString(removeDuplicatesNotModifyingInput(new int[]{-1,-1,0,0,2,3,3,4,7,9,9,13,15,15,15,15,15,15})));
+        System.out.println(Arrays.toString(removeDuplicatedElement(new int[]{-1,-1,0,0,2,3,3,4,7,9,9,13,15,15,15,15,15,15})));
+        System.out.println(Arrays.toString(removeElement(new int[]{-1,-1,0,0,2,3,3,4,7,9,9,13,15,15,15,15,15,15},15)));
     }
 }
