b8c3b61c9f99d92d3358e1bd972f7bfbeab3d31b
Shivanshu_Gupta <gupta_shivanshu@yahoo.co.in>
2019-12-30 01:33:49 -0600

    LinkedListNode

---
+++ java-practice/src/main/java/com/shivanshu/practice/LinkedListNode.java
@@ -0,0 +1,13 @@
+package com.shivanshu.practice;
+
+public class LinkedListNode {
+
+    int data;
+    LinkedListNode next;
+
+
+    public LinkedListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
